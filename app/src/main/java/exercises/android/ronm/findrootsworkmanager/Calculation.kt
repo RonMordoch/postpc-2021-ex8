3fec76d12e744112c316aeb8f542fe3a1e9aea7e
mordoch-ron <ron.mordoch@mail.huji.ac.il>
2021-06-02 11:42:42 +0300

    Cleaner code in Calculation.kt setters, added unit tests, exercise done

--- app/src/main/java/exercises/android/ronm/findrootsworkmanager/Calculation.kt
+++ app/src/main/java/exercises/android/ronm/findrootsworkmanager/Calculation.kt
@@ -6,16 +6,22 @@ data class Calculation(val id: UUID, val number: Long) : Comparable<Calculation>
 
     var root1: Long = number
     var root2: Long = 1L
+
     var progress: Int = MIN_PROGRESS
+        set(value) {
+            field = value
+            isCalculating = (value != MAX_PROGRESS)
+        }
+
     var isNumberPrime: Boolean = false
+
     var isCalculating: Boolean = true
         set(value) {
             field = value
-            if (!value) { // finished calculating, set progress to max
-                progress = MAX_PROGRESS
-                if (root1 == 1L || root2 == 1L) { // one of the roots found in result is 1, therefore the number is prime
-                    isNumberPrime = true
-                }
+            if (!value) {
+                // don't update progress here to avoid recursive setters calls
+                // one of the roots found in result is 1, therefore the number is prime
+                isNumberPrime = (root1 == 1L || root2 == 1L)
             }
         }
 
@@ -23,11 +29,11 @@ data class Calculation(val id: UUID, val number: Long) : Comparable<Calculation>
     override fun compareTo(other: Calculation): Int {
         // this object is in-progress, other is done, this object is less than other
         if (isCalculating && !other.isCalculating) {
-            return -1
+            return LESS_THAN
         }
         // this object is done, other is in-progress, other is less than this object
         else if (!isCalculating && other.isCalculating) {
-            return 1
+            return GREATER_THAN
         }
         // else, objects are both in-progress or both are done, sort by number from smallest to largest
         return number.compareTo(other.number)
@@ -36,5 +42,9 @@ data class Calculation(val id: UUID, val number: Long) : Comparable<Calculation>
     companion object {
         private const val MAX_PROGRESS = 100
         private const val MIN_PROGRESS = 0
+        private const val LESS_THAN = -1
+        private const val GREATER_THAN = 1
     }
+
+
 }
---
+++ app/src/test/java/exercises/android/ronm/findrootsworkmanager/CalculationTest.kt
@@ -0,0 +1,74 @@
+package exercises.android.ronm.findrootsworkmanager
+
+import org.junit.Assert
+import org.junit.Test
+import java.util.*
+
+class CalculationTest {
+
+    @Test
+    fun when_updatingProgressBelowMaxProgress_then_stateShouldBeCalculating(){
+        val calculation = Calculation(UUID.randomUUID(), 5)
+        calculation.progress = 50
+        Assert.assertTrue(calculation.isCalculating)
+    }
+
+    @Test
+    fun when_updatingProgressAsMaxProgress_then_stateShouldBeDone(){
+        val calculation = Calculation(UUID.randomUUID(), 5)
+        calculation.progress = 50
+        Assert.assertTrue(calculation.isCalculating)
+        calculation.progress = 100
+        Assert.assertFalse(calculation.isCalculating)
+    }
+
+    @Test
+    fun when_rootsAreFound_numberIsPrime_isFalse(){
+        val calculation = Calculation(UUID.randomUUID(), 6)
+        calculation.root1 = 2L
+        calculation.root2 = 3L
+        calculation.progress = 100
+        Assert.assertFalse(calculation.isNumberPrime)
+    }
+
+    @Test
+    fun when_rootsAreNotFound_numberIsPrime_isTrue(){
+        val calculation = Calculation(UUID.randomUUID(), 7)
+        calculation.root1 = 1L
+        calculation.root2 = 7L
+        calculation.progress = 100
+        Assert.assertTrue(calculation.isNumberPrime)
+    }
+
+
+    @Test
+    fun testCompareTo_with_twoInProgressCalculations(){
+        val calculation1 = Calculation(UUID.randomUUID(), 7)
+        val calculation2 = Calculation(UUID.randomUUID(), 9)
+        Assert.assertEquals(LESS_THAN, calculation1.compareTo(calculation2))
+    }
+
+    @Test
+    fun testCompareTo_with_twoDoneCalculations(){
+        val calculation1 = Calculation(UUID.randomUUID(), 12)
+        calculation1.isCalculating = false
+        val calculation2 = Calculation(UUID.randomUUID(), 9)
+        calculation2.isCalculating = false
+        Assert.assertEquals(GREATER_THAN, calculation1.compareTo(calculation2))
+    }
+
+    @Test
+    fun testCompareTo_with_oneCalcInProgress_and_oneCalcDone(){
+        val calculationDone = Calculation(UUID.randomUUID(), 7)
+        calculationDone.isCalculating = false
+        val calculationInProgress = Calculation(UUID.randomUUID(), 9)
+        Assert.assertEquals(GREATER_THAN, calculationDone.compareTo(calculationInProgress))
+    }
+
+
+    companion object {
+        private const val LESS_THAN = -1
+        private const val GREATER_THAN = 1
+    }
+
+}
